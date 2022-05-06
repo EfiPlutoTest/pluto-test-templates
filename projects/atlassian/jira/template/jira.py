d81a5691e44d162bf6534cb6ff3918985ce2f7c2
aromu <aapo.romu@eficode.com>
2022-05-06 16:57:59 +0300

    Template execution result implementation

--- projects/atlassian/jira/template/jira.py
+++ projects/atlassian/jira/template/jira.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import json
 import logging as log
 from utils.common import LogManager
 
@@ -9,5 +10,13 @@ if __name__ == '__main__':
     log_manager = LogManager()
     log_manager.init_logging()
     log.info("Hello World!")
-
     log.info("Environment: %s", os.environ)
+
+    result = {'result': {'projectKey': 'TSTPRJ',
+                         'url': 'http://jira.yourcompany.example.com'
+                         },
+              'errors': ['Something weird happened when creating your project but it seems to somewhat be there']
+              }
+
+    with open('jira.py.result.json', 'w', encoding="utf8") as target_file:
+        target_file.write(json.dumps(result))
