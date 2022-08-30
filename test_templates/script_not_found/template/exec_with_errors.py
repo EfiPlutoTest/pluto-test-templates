7090774892dfd9bac61ff007848f16cd2b6a15f3
Aapo Romu <aapo.romu@eficode.com>
2022-08-30 16:30:48 +0300

    Added notfound test template

---
+++ test_templates/script_not_found/template/exec_with_errors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import json
+import logging as log
+from utils.common import LogManager
+
+if __name__ == '__main__':
+    print("RUNNING....")
+    log_manager = LogManager()
+    log_manager.init_logging()
+    log.info("Hello World!")
+    log.info("Environment: %s", os.environ)
+
+    result = {'result': {'projectKey': 'ERRPRJ',
+                         'url': 'http://jira.yourcompany.example.com'
+                         },
+              'errors': ['Something weird happened when creating your project but it seems to somewhat be there']
+              }
+
+    with open(os.environ['RESULT_FILE'], 'w', encoding="utf8") as target_file:
+        target_file.write(json.dumps(result))
