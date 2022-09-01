de3f85f36579f7814c6e7f480b66dee1dc0aa1be
Aapo Romu <aapo.romu@eficode.com>
2022-09-01 12:50:09 +0300

    Improve input validation test

--- test_templates/inputs_n_outputs/template/take_input_produce_output.py
+++ test_templates/inputs_n_outputs/template/take_input_produce_output.py
@@ -14,8 +14,12 @@ if __name__ == '__main__':
 
     result = {'result': {
         'projectKey': 'TSTPRJ',
-        'url': 'http://jira.yourcompany.example.com'
-    }}
+        'url': 'http://jira.yourcompany.example.com',
+        'bool_result': True,
+        'false_result': False,
+        'number_1_result': os.environ['NUMBER_1'],
+        'number_2_result': os.environ['NUMBER_2']}
+    }
 
     with open(os.environ['RESULT_FILE'], 'w', encoding="utf8") as target_file:
         target_file.write(json.dumps(result))
